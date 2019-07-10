57dc616162191b95829429b89fb3945762cd2150
Gilad Barnea <giladbrn@gmail.com>
2019-07-10 16:12:19 +0300

    tone 13.8.18, Piano.ts no errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,7 @@ module.exports = {
     "env": {
         "browser": true,
         "node" : true,
-        "mocha" : true,
+		"mocha" : true,
         "es6": true,
 		"amd" : false,
     },
--- src/typing.d.ts
+++ src/typing.d.ts
@@ -1,14 +1,14 @@
-declare interface PianoOptions {
-    velocities : number,
-    minNote : number,
-    maxNote : number,
-    release : boolean,
-    pedal : boolean,
-    samples : string,
-    volume : {
-        pedal : number,
-        strings : number,
-        keybed : number,
-        harmonics : number
+declare interface PianoOptions extends AudioContext {
+    velocities: number,
+    minNote: number,
+    maxNote: number,
+    release: boolean,
+    pedal: boolean,
+    samples: string,
+    volume: {
+        pedal: number,
+        strings: number,
+        keybed: number,
+        harmonics: number
     }
 }
